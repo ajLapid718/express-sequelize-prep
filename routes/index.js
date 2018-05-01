6cceac18eae4cffbd0ddf96ccc8bbe2782ae3cbb
Allan James Lapid <ajLapid718@gmail.com>
2018-05-01 18:28:14 -0400

    responds with an array of all tweets

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,8 @@ var router = require('express').Router();
 var db = require('../models/');
 var Tweet = db.model('tweet');
 
+router.get("/tweets", function(req, res, next) {
+  res.json([]);
+})
+
 module.exports = router;
