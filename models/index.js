1dcb63610d98d294b779f85dfeed110c96ec9f1d
Allan James Lapid <ajLapid718@gmail.com>
2018-05-01 16:54:23 -0400

    pass two out of thirteen specs

--- models/index.js
+++ models/index.js
@@ -2,7 +2,9 @@ var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/express_sequelize_review', { logging: false });
 
 var Tweet = db.define('tweet', {
-
+  text: {
+    type: Sequelize.STRING
+  }
 });
 
 var User = db.define('user', {
