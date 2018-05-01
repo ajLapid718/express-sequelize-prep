867db27922af44097bc600905ac3de17f3f44367
Allan James Lapid <ajLapid718@gmail.com>
2018-05-01 18:58:05 -0400

    returns proper hashtagged tweets if query

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var db = require('../models/');
 var Tweet = db.model('tweet');
 
 router.get("/tweets", function(req, res, next) {
-  res.json([]);
-})
+  if (req.query.hashtag) {
+    let hashtagToSearchFor = req.query.hashtag;
+    Tweet.findByHashtag(hashtagToSearchFor).then(function(tweets) {
+      res.json(tweets);
+    });
+  } else {
+    Tweet.findAll().then(function(tweets) {
+      res.json(tweets);
+    }).catch(err => console.log(err));
+  }
+});
 
 module.exports = router;
